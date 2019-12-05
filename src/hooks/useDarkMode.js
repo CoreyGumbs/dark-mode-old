5383381b602685da64796759e1ad3262c6acc6fd
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-12-05 17:01:36 -0500

    [Add] useDarkMode Hook completed, not working

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,17 @@
+import {useEffect} from 'react';
+
 import {useLocalStorage} from './useLocalStorage';
 
-const useDarkMode = () => {
-    
+export const useDarkMode = ()  => {
+   const [enabled, setEnabled] = useLocalStorage('enable');
+
+   useEffect(() => {
+        if(window.localStorage.getItem(enabled) === true){
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
+   }, [enabled]);
+
+   return [enabled, setEnabled];
 }
